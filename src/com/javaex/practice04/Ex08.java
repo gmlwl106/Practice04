000507b9d030c3bcb1f6ff76d1fe3772733fab93
gmlwl106 <66778894@naver.com>
2022-04-20 17:29:54 +0900

    Practice04. Ex08-미니로또(중복확인)

---
+++ src/com/javaex/practice04/Ex08.java
@@ -0,0 +1,21 @@
+package com.javaex.practice04;
+
+public class Ex08 {
+	public static void main(String[] args) {
+		
+		int[] lotto = new int[6];
+		
+		for(int i=0; i<lotto.length; i++) { //lotto 배열에 난수 생성
+			lotto[i] = (int)(Math.random()*45)+1;
+			
+			for(int j=0; j<=i; j++) { //중복이 있는지 검사
+				if(lotto[i] == lotto[j]) {
+					lotto[j] = (int)(Math.random()*45)+1; //중복시 난수 재생성
+				}
+			}
+			
+			System.out.print(lotto[i] + "\t"); //lotto 배열 출력
+		}
+		
+	}
+}
